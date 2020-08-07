c42b53228033c0496d2303475da6509f712a28bb
Markus Pöschl <Poeschl@users.noreply.github.com>
2020-08-07 22:21:48 +0200

    :Add start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+python3.8 -m venv venv
+source venv/bin/activate
+
+pip3 install -r requirements.txt
+
+PYTHONPATH="$(printf "%s:" orgabot/)"
+export PYTHONPATH=$PYTHONPATH PYTHONUNBUFFERED=1
+python3 -u orgabot/orgabot.py
